17dcea54005da63b3eb23212629b0223317fa88e
Danny White <hi@dannywhite.org>
2023-01-03 09:59:29 +1000

    feat: randomly select only one item across all pages

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import requests
 import math
+import random
 
 # itemsPerPage = 10
-limit = 3  # Breaks if over 100
+limit = 10  # Breaks if over 100
 
 fields = "id,image_id,title,artist_id,medium_display"
 url = f"https://api.artic.edu/api/v1/artworks/search?limit={limit}&fields={fields}"
@@ -18,9 +19,9 @@ searchQuery = {
                 # {"match": {"subject_titles": "geometric"}},
                 # {"range": {"color.h": {"lt": 2}}},
             ],
-            "should": [
-                {"term": {"is_boosted": True}},
-            ],
+            # "should": [
+            #     {"term": {"is_boosted": True}},
+            # ],
             # "must_not": [
             #     {"match": {"medium_display": "Ceramic and pigment"}},
             #     {"match": {"medium_display": "Plant fibers"}},
@@ -34,62 +35,32 @@ searchQuery = {
 # classification_titles: "etching"
 # foul-biting
 
-# searchQuery = {
-#     "query": {
-#         "bool": {
-#             "must": [
-#                 {"term": {"artist_id": 36198}},
-#                 {"term": {"is_public_domain": True}},
-#                 {"match": {"place_of_origin": "Spain"}},
-#             ],
-#             "should": [
-#                 {"term": {"is_boosted": True}},
-#             ],
-#         }
-#     }
-# }
-
-# searchQuery = {
-#     "query": {
-#         "bool": {
-#             "must": {
-#                 "match": {
-#                     "place_of_origin": "Spain"
-#                 }
-#             }
-#         }
-#     }
-# }
-
-
-# searchQuery = {
-#     "query": {
-#         "bool": {
-#             "must": [
-#                 {"term": {"artist_id": 36198}},
-#                 {"term": {"place_of_origin": "Spain"}},
-#                 {"term": {"is_public_domain": True}},
-#             ]
-#         }
-#     }
-# }
 
 print("———————————————————")
 r = requests.post(url, headers=headers, json=searchQuery)
 art = r.json()
 # print(art)
 resultSize = art["pagination"]["total"]
-pages = int(math.ceil(resultSize / 10))
+pages = int(math.ceil(resultSize / 10))  # Fixed to 10 items per page?
 
-size = 360
+# Crop...
+canvasSize = 360
 # imageParams = "/full/843,/0/default.jpg"
-imageParams = f"/square/{size},/0/bitonal.jpg"  # default.jpg, gray.jpg. Can I get ImageMagick or PIL to work on the monochrome a bit better? Stippling
+imageParams = f"/square/{canvasSize},/0/bitonal.jpg"  # default.jpg, gray.jpg. Can I get ImageMagick or PIL to work on the monochrome a bit better? Stippling
 
 # Get results
-print(
-    f"{resultSize} total results across {pages} pages, limited to {len(art['data'])}:"
-)
-for i in range(len(art["data"])):
-    imageId = art["data"][i]["image_id"]
-    print(f"{art['data'][i]['title']}, {art['data'][i]['id']}:")
-    print(f"https://www.artic.edu/iiif/2/{imageId}{imageParams}")
+print(f"Randomly selecting one of {resultSize} possible choices...")
+
+
+randomPage = random.randint(1, pages)
+print(f"Randomly selected page {randomPage} of {pages}")
+
+url = f"https://api.artic.edu/api/v1/artworks/search?limit={limit}&page={randomPage}&fields={fields}"
+r = requests.post(url, headers=headers, json=searchQuery)
+art = r.json()
+randomArt = random.randint(0, limit)
+print(f"Randomly selected item {randomArt} of {limit}")
+
+imageId = art["data"][randomArt]["image_id"]
+print(f"{art['data'][randomArt]['title']}, {art['data'][randomArt]['id']}:")
+print(f"https://www.artic.edu/iiif/2/{imageId}{imageParams}")

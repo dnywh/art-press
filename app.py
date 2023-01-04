9bce0c30894e614c03850c4a0b0f0f0731ae5284
Danny White <hi@dannywhite.org>
2023-01-04 16:03:54 +1000

    feat: shared optical offsets and sizing module

--- app.py
+++ app.py
@@ -16,24 +16,29 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Prepare directories so they can be reached from anywhere
 appDir = os.path.dirname(os.path.realpath(__file__))
+assetsDir = os.path.join(appDir, "assets")
 # Get required items from other root-level directories
 parentDir = os.path.dirname(appDir)
 libDir = os.path.join(parentDir, "lib")
 if os.path.exists(libDir):
     sys.path.append(libDir)
-# Change the below to whatever Waveshare model you have, or add a different display's driver to /lib
-from waveshare_epd import (
-    epd7in5_V2,
-)
+
+# Change the below import to match your display's driver
+from waveshare_epd import epd5in83_V2 as display
+
+# Adjust your optical offsets from one place
+import layout
 
 # Set design basics
-canvasSize = 360
-bufferX = 4
-bufferY = 14
-imageQuality = "bitonal"  # default.jpg, gray.jpg, bitonal.jpg. Can I get ImageMagick or PIL to work on the monochrome a bit better? Stippling
-preferCrop = True
+containerSize = layout.size
+offsetX = layout.offsetX
+offsetY = layout.offsetY
+imageQuality = "bitonal"  # Options are "default", "gray", "bitonal"
+preferCrop = True  # Crop to center of original image if true
 imageWidth = 843  # Preferred width as per ARTIC API documentation
 exportImages = True  # Save both the input and output image in an exports folder
+header = {"AIC-User-Agent": "art-box (endless.paces-03@icloud.com)"}  # As a courtesy
+pageItemLimit = 10  # 100 or less per page
 
 criteria = {
     "query": {
@@ -63,47 +68,46 @@ try:
     # Local time
     timeStampNice = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
     logging.info(f"Kicking off at {timeStampNice}")
-    # Fields to include in data
-    fields = "api_link,image_id,title,artist_id,artist_title,medium_display,thumbnail"  # Make sure to include any fields that are queried later on
-    limit = 10  # Breaks if over 100
-    url = f"https://api.artic.edu/api/v1/artworks/search?limit={limit}&fields={fields}"
-    headers = {"user-agent": "test-app/endless.paces-03@icloud.com"}
-
-    r = requests.post(url, headers=headers, json=criteria)
+    # Post a bare minimum query to find out the amount of pages the criteria returns
+    url = f"https://api.artic.edu/api/v1/artworks/search?limit=0"
+    r = requests.post(url, headers=header, json=criteria)
     art = r.json()
+    # Get results
     resultSize = art["pagination"]["total"]
     pages = int(math.ceil(resultSize / 10))  # Fixed to 10 items per page?
-
-    # Get results
     logging.info(f"There are {resultSize} possible choices for your criteria")
-
+    # Select a random page
     randomPage = random.randint(0, pages - 1)
     logging.info(f"Randomly selected page {randomPage + 1} of {pages}")
 
-    url = f"https://api.artic.edu/api/v1/artworks/search?limit={limit}&page={randomPage}&fields={fields}"
-    r = requests.post(url, headers=headers, json=criteria)
+    # Post the full query into this page
+    # Fields to include in data
+    fields = "api_link,image_id,title,artist_id,artist_title,medium_display,thumbnail"  # Make sure to include any fields that are queried later on
+    url = f"https://api.artic.edu/api/v1/artworks/search?limit={pageItemLimit}&page={randomPage}&fields={fields}"
+    r = requests.post(url, headers=header, json=criteria)
     art = r.json()
-    randomArt = random.randint(0, limit - 1)
-    logging.info(f"Randomly selected item {randomArt + 1} of {limit}")
-
+    # Select a random item
+    randomArt = random.randint(0, pageItemLimit - 1)
+    logging.info(f"Randomly selected item {randomArt + 1} of {pageItemLimit}")
+    # Parse random item
     apiLink = art["data"][randomArt]["api_link"]
     imageId = art["data"][randomArt]["image_id"]
     imageTitle = art["data"][randomArt]["title"]
     imageArtist = art["data"][randomArt]["artist_title"]
 
-    # Crop
+    # Crop artwork
     if preferCrop == True:
         # Calculate center crop of image
         imageWidth = art["data"][randomArt]["thumbnail"]["width"]
         imageHeight = art["data"][randomArt]["thumbnail"]["height"]
-        cropStartX = int((imageWidth - canvasSize) / 2)
-        cropStartY = int((imageHeight - canvasSize) / 2)
+        cropStartX = int((imageWidth - containerSize) / 2)
+        cropStartY = int((imageHeight - containerSize) / 2)
         # Pass this crop region into the image parameters
-        cropRegion = f"{cropStartX},{cropStartY},{canvasSize},{canvasSize}"
-        imageParams = f"/{cropRegion}/{canvasSize},/0/{imageQuality}.jpg"
+        cropRegion = f"{cropStartX},{cropStartY},{containerSize},{containerSize}"
+        imageParams = f"/{cropRegion}/{containerSize},/0/{imageQuality}.jpg"
     else:
         # Just get the square version of the image, resized to canvas size
-        imageParams = f"/square/{canvasSize},/0/{imageQuality}.jpg"
+        imageParams = f"/square/{containerSize},/0/{imageQuality}.jpg"
 
     artworkUrl = f"https://www.artic.edu/iiif/2/{imageId}{imageParams}"
     canonicalArtworkUrl = (
@@ -113,14 +117,6 @@ try:
     logging.info(f"Artwork URL: {artworkUrl}")
 
     timeStampSlugToMin = datetime.today().strftime("%Y-%m-%d-%H-%M")
-    # Prepare directory for saving image(s), if applicable
-    exportsDir = os.path.join(appDir, "exports")
-    imageDir = os.path.join(exportsDir, timeStampSlugToMin)
-    if exportImages == True:
-        if not os.path.exists(exportsDir):
-            os.makedirs(exportsDir)
-        if not os.path.exists(imageDir):
-            os.mkdir(imageDir)
 
     # Download a temporary copy of the artwork to render to screen. Store it locally
     artworkImagePath = os.path.join(appDir, "artwork.jpg")
@@ -134,33 +130,39 @@ try:
     # Throw away original artwork image since new versions have been made
     os.remove(artworkImagePath)
 
-    # Save out
-    if exportImages == True:
-        # Save out image in its directory
-        artwork.save(os.path.join(imageDir, f"{timeStampSlugToMin}.jpg"))
-
     # Log information, including a URL for the original artwork
     output = f"Printed at:\t{timeStampNice}\nTitle:\t\t{imageTitle}\nArtist:\t\t{imageArtist}\nAPI URL:\t{apiLink}\nImage URL:\t{canonicalArtworkUrl}"
     # ...to console
     logging.info(f"\n{output}")
-    # ...to image directory, if applicable
+
+    # Save out
     if exportImages == True:
+        # Prepare directory for saving image(s)
+        exportsDir = os.path.join(appDir, "exports")
+        imageDir = os.path.join(exportsDir, timeStampSlugToMin)
+        if not os.path.exists(exportsDir):
+            os.makedirs(exportsDir)
+        if not os.path.exists(imageDir):
+            os.mkdir(imageDir)
+        # Save image in its directory
+        artwork.save(os.path.join(imageDir, f"{timeStampSlugToMin}.jpg"))
+        # Also save text output
         with open(os.path.join(imageDir, f"{timeStampSlugToMin}.txt"), "w") as f:
             f.write(output)
 
     # Start rendering
-    epd = epd7in5_V2.EPD()
+    epd = display.EPD()
     epd.init()
     epd.Clear()
 
     canvas = Image.new("1", (epd.width, epd.height), "white")
     draw = ImageDraw.Draw(canvas)
 
-    # Center grid
-    offsetX = int((epd.width - canvasSize) / 2) + bufferX
-    offsetY = int((epd.height - canvasSize) / 2) + bufferY
+    # Calculate top-left starting position
+    startX = offsetX + int((epd.width - containerSize) / 2)
+    startY = offsetY + int((epd.height - containerSize) / 2)
 
-    canvas.paste(artwork, (offsetX, offsetY))
+    canvas.paste(artwork, (startX, startY))
 
     # Render all of the above to the display
     epd.display(epd.getbuffer(canvas))
@@ -178,5 +180,5 @@ except IOError as e:
 # Exit plan
 except KeyboardInterrupt:
     logging.info("Exited.")
-    epd7in5_V2.epdconfig.module_exit()
+    display.epdconfig.module_exit()
     exit()
